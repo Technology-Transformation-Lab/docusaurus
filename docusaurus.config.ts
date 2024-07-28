f24ff709e278bd2d42a8ae293242671aaf165198
Lukas <endless19990816@gmail.com>
2024-07-28 17:52:33 +0800

    change base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
   url: 'https://technology-transformation-lab.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // TODO: If change baseURL we need to change repo name together
+  baseUrl: '/docusaurus',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
